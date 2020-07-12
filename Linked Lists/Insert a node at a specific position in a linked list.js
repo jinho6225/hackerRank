116c9dba41411b907c7c4d3390b32c08484450b4
jinho6225 <jinho6225@hotmail.com>
2020-07-12 12:14:56 -0700

    solve interview preparation kit

---
+++ Linked Lists/Insert a node at a specific position in a linked list.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtPosition(head, data, position) {
+    let cur = head
+    let count = 0 //16-13-7
+    while (cur) {
+        if (count === position-1) {
+            let last = cur.next
+            let temp = new SinglyLinkedListNode(data)
+            cur.next = temp
+            temp.next = last
+            break;
+        }
+        count++
+        cur = cur.next
+    }
+    return head
+}
+
+function main() {
---
+++ String Manipulation/Strings: Making Anagrams.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map((str) => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the makeAnagram function below.
+function makeAnagram(a, b) {
+  let map = {};
+  for (let i = 0; i < a.length; i++) {
+    if (!map[a[i]]) {
+      map[a[i]] = 1;
+    } else {
+      map[a[i]]++;
+    }
+  }
+  let str = '';
+  for (let i = 0; i < b.length; i++) {
+    if (map[b[i]]) {
+      if (map[b[i]] > 1) {
+        map[b[i]]--;
+      } else if (map[b[i]] === 1) {
+        delete map[b[i]];
+      }
+    } else {
+      str += b[i];
+    }
+  }
+  let count = 0;
+  for (let key in map) {
+    count += map[key];
+  }
+  return count + str.length;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const a = readLine();
+
+  const b = readLine();
+
+  const res = makeAnagram(a, b);
+
+  ws.write(res + '\n');
+
+  ws.end();
+}
