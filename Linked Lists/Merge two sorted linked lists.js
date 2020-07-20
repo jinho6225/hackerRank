8106805053b985da0f4930282bf03c16c9b6e165
jinho6225 <jinho6225@hotmail.com>
2020-07-20 16:19:19 -0700

    another solution added

--- Linked Lists/Merge two sorted linked lists.js
+++ Linked Lists/Merge two sorted linked lists.js
@@ -98,3 +98,41 @@ function mergeLists(head1, head2) {
 }
 
 function main() {
+
+
+
+// Another solution added
+// Complete the mergeLists function below.
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+    let node = new SinglyLinkedListNode();
+    let currentNode = node;
+    while (head1 !== null && head2 !== null) {
+        if (head1.data <= head2.data) {
+        currentNode.next = new SinglyLinkedListNode(head1.data);
+        head1 = head1.next;
+        } else {
+        currentNode.next = new SinglyLinkedListNode(head2.data);
+        head2 = head2.next;
+        }
+        currentNode = currentNode.next;
+    }
+    if (head2 !== null) {
+        currentNode.next = head2;
+    }
+    if (head1 !== null) {
+        currentNode.next = head1;
+    }
+    node = node.next;
+    return node;
+}
+
+function main() {
