450e44550d91dff4fd8a9b3a77d8cfaaf753ea62
jinho6225 <jinho6225@hotmail.com>
2020-07-29 11:08:50 -0700

    delete duplicate-value nodes from a sorted linked list

---
+++ Linked Lists/Delete duplicate-value nodes from a sorted linked list.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function removeDuplicates(head) {
+    let cur = head
+    let prev = null
+    while(cur) {
+        if (prev) {
+            if (cur.data === prev.data) {
+                prev.next = cur.next
+            } else {
+                prev = cur
+            }
+        } else {
+            prev = cur
+        }
+        cur = cur.next
+    }
+    return head
+}
+
+function main() {
