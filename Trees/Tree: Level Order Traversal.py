648c1036cb3ed520dbf096c364e2d3bce33c57de
jinho6225 <jinho6225@hotmail.com>
2020-07-29 08:39:21 -0700

    level order traversal, BFS

---
+++ Trees/Tree: Level Order Traversal.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    #Write your code here
+    lst = []
+    queue = []
+    queue.append(root)
+    while len(queue):
+        cur = queue.pop(0)
+        lst.append(str(cur.info))
+        if cur.left: queue.append(cur.left)
+        if cur.right: queue.append(cur.right)
+    print(' '.join(lst))
